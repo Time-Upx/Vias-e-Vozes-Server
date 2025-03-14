5a95c0c645ec2d16fc05ecae82b0c0184b4c1e3c
Gabriel Rosa <gabriel.rosa.080305@gmail.com>
2025-03-14 20:00:59 -0300

    Preparing Spring enviroment

---
+++ src/test/java/TimeUpx/ViasEVozes/Backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package TimeUpx.ViasEVozes.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
